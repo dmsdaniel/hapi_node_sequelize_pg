5d3da6d0912c30a1b0b03be49bcb546c65de6894
Daniel Santos <dmsdaniel@gmail.com>
2020-11-30 23:16:38 -0300

    modified:   crud/frontend/.eslintcache 	modified:   crud/frontend/src/components/template/Header.css

--- crud/frontend/src/components/template/Logo.jsx
+++ crud/frontend/src/components/template/Logo.jsx
@@ -1,12 +1,12 @@
 import './Logo.css'
 import React, { memo } from 'react'
-import logo from '../../assets/images/logo.jpg'
+import logo from '../../assets/images/x64.png'
 
 const Logo = () => {
     return (
         <aside className="logo">
             <a href="/" className="logo">
-                <img src={logo} alt="logo" />
+                <img src={logo} alt="logo"/>
             </a>
         </aside>
     )
--- crud/frontend/src/main/App.jsx
+++ crud/frontend/src/main/App.jsx
@@ -5,7 +5,6 @@ import React, { memo } from 'react'
 import Logo from '../components/template/Logo'
 import Nav from '../components/template/Nav'
 import Main from '../components/template/Main'
-import Header from '../components/template/Header'
 import Footer from '../components/template/Footer'
 
 const App = () => {
